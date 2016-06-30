fe86c4eb83f4c108a86cc4c2e7381215ebb67ee6
Tomomi Imura <tomomi.i@gmail.com>
2016-06-30 14:23:30 -0700

    Update the code to v.2. See README for all the changes

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Poor Man's HUE Smart Light Bulb Demo
-## with Arduino, Common Cathode RGB LED, JOhnny-Five, and PubNub
+## with Arduino, Common Cathode RGB LED, Johnny-Five, and PubNub
 
-I'll write more later!
+The tutorial is available on [Tuts+](http://code.tutsplus.com/tutorials/how-to-create-a-smart-device-with-arduino-and-nodejs-using-pubnub--cms-25508).
+
+The Tuts+ tutorial was written with v1 code, so some code snippets in the tutorial do not match in the latest code in this repo. See the changes below.
+
+### Changes in v2
+
+- Instead of sending each color value like `{color:red, brightness: 123}`, now all color values is sent as one object at a time, as `{r: 123, g: 98, b: 0}`
+- Added `subscribe()` in the front-end code to reset the UI slider position every time other subscribers make changes
+- Added `history()` in the front-end code to initialize the UI slider's initial state by reading the very last values from history
+- Added `history()` in the node.js code to initialize the LED color state at the initial load
--- node/index.js
+++ node/index.js
@@ -1,15 +1,10 @@
 var five = require('johnny-five');
 
-var led;
-var r = 0;
-var b = 0;
-var g = 0;
-
 five.Board().on('ready', function() {
   console.log('ready');
 
   // Initialize the RGB LED
-  led = new five.Led.RGB({
+  var led = new five.Led.RGB({
     pins: {
       red: 6,
       green: 5,
@@ -17,31 +12,39 @@ five.Board().on('ready', function() {
     }
   });
 
-  // Turn it on and set the initial color
   led.on();
-  led.color({red: r, blue: b, green: g});
+  led.color({red: 0, blue: 0, green: 0});
+
+  var pubnub = require('pubnub').init({
+    publish_key: 'pub-c-0b43969b-341d-41f5-a85e-0bd9d30404b8',
+    subscribe_key: 'sub-c-cb24903e-c9f4-11e5-b684-02ee2ddab7fe'
+  });
+
+  var channel = 'hue-clone';
+
+  pubnub.subscribe({
+    channel: channel,
+    callback: setLedColor,
+    connect: initLedColor,
+    error: function(err) {console.log(err);}
+  });
+
+  function setLedColor(m) {
+    led.color({red: m.r, blue: m.b, green: m.g});
+    console.log( 'color change to...' );
+    console.log( led.color() );
+  }
+
+  function initLedColor() {
+    pubnub.history({
+      channel: channel,
+      count: 1,
+      callback: function(messages) {
+        messages[0].forEach(function(m) {
+          setLedColor(m);
+        });
+      }
+    });
+  }
+
 });
-
-var pubnub = require('pubnub').init({
-  subscribe_key: 'demo',
-  publish_key:   'demo'
-});
-var channel = 'hue-clone';
-
-pubnub.subscribe({
-  channel: channel,
-  callback: function(m) {
-    if(led) {
-      r = (m.color === 'red') ? m.brightness : r;
-      g = (m.color === 'green') ? m.brightness : g;
-      b = (m.color === 'blue') ? m.brightness : b;
-
-      led.color({red: r, blue: b, green: g});
-
-      console.log( 'color change to...' );
-      console.log( led.color() );
-    }
-  },
-  error: function(err) {console.log(err);}
-});
-
