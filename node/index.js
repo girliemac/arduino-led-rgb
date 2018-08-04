4d0014031a8e56ed6304873208a15cee0d1acbad
Craig Conover <cvconover@gmail.com>
2018-08-04 09:30:31 -0700

    1st draft untested v4 migration - history may not work properly and cause error

--- node/index.js
+++ node/index.js
@@ -1,4 +1,6 @@
 var five = require('johnny-five');
+var PubNub = require('pubnub'); // ES5
+// import PubNub from 'pubnub'; // ES6
 
 five.Board().on('ready', function() {
   console.log('ready');
@@ -15,18 +17,54 @@ five.Board().on('ready', function() {
   led.on();
   led.color({red: 0, blue: 0, green: 0});
 
-  var pubnub = require('pubnub').init({
-    publish_key: 'pub-c-0b43969b-341d-41f5-a85e-0bd9d30404b8',
-    subscribe_key: 'sub-c-cb24903e-c9f4-11e5-b684-02ee2ddab7fe'
+  var pubnub = new PubNub({
+    publishKey: 'pub-c-0b43969b-341d-41f5-a85e-0bd9d30404b8',
+    subscribeKey: 'sub-c-cb24903e-c9f4-11e5-b684-02ee2ddab7fe',
+    ssl: true
   });
 
   var channel = 'hue-clone';
 
+  pubnub.addListener({
+      message: function(message) {
+        setLedColor(message);
+      },
+      status: function(statusEvent) {
+        // subscribed (connected) to channel was successful
+        if (statusEvent.category === "PNConnectedCategory") {
+          console.log("connected to " + channel);
+          initLedColor();
+        }
+
+        // you may want to handle each category uniquely. 
+        // the other non error status will be logged to console in the "else" below
+        else if (statusEvent.category === "PNNetworkIssuesCategory") {
+          console.error(statusEvent);
+        }
+        else if (statusEvent.category === "PNNetworkDownCategory") {
+          console.error(statusEvent);
+        }
+        else if (statusEvent.category === "PNAccessDeniedCategory") {
+          console.error(statusEvent);
+        }
+        else if (statusEvent.category === "PNMalformedResponseCategory") {
+          console.error(statusEvent);
+        }
+        else if (statusEvent.category === "PNBadRequestCategory") {
+          console.error(statusEvent);
+        }
+        else if (statusEvent.category === "PNTimeoutCategory") {
+          console.error(statusEvent);
+        }
+        // other non error status events
+        else {
+          console.log(statusEvent);
+        }
+      }
+  });
+
   pubnub.subscribe({
-    channel: channel,
-    callback: setLedColor,
-    connect: initLedColor,
-    error: function(err) {console.log(err);}
+    channels: [channel]
   });
 
   function setLedColor(m) {
@@ -36,15 +74,23 @@ five.Board().on('ready', function() {
   }
 
   function initLedColor() {
-    pubnub.history({
-      channel: channel,
-      count: 1,
-      callback: function(messages) {
-        messages[0].forEach(function(m) {
-          setLedColor(m);
-        });
+    pubnub.history(
+      {
+          channel: channel,
+          count: 1
+      },
+      function (status, response) {
+        var msgs = response.messages;
+        if (msgs != undefined && msgs.length > 0) {
+          // only 1 message fetched, so use element 0 to get it
+          msgs[0].forEach(function(m) {
+            // loop through the elements of the "brightness" message: r, g & b
+            // and set the LED color
+            setLedColor(m);
+          });
+        }
       }
-    });
+    );
   }
-
-});
+  
+}
